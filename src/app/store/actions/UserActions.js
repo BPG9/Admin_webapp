c9d982d9b1ec3c91836204b09c2e6974d751c6e1
Shayan <shayandavarifard@yahoo.com>
2020-03-31 18:43:50 +0200

    != "null"  fixen bei Login

--- src/app/store/actions/UserActions.js
+++ src/app/store/actions/UserActions.js
@@ -90,7 +90,7 @@ export const AuthLogin = (id, pass) => {
 
         axios.post("http://247-244.gugw.tu-darmstadt.de/web", request.login(id, pass), config)
             .then(x => {
-                if (x.data.data.auth.refreshToken || x.data.data.auth.accessToken) {
+                if (x.data.data.auth.refreshToken != "null" || x.data.data.auth.accessToken != "null") {
                     alert("Error gibt es keine benutzer oder Pass ist falsch")
                 }
                 else {
